d1b418d4c3d23f1784939fb9d6dd0cbf152380f3
lonelyobserver <lcmulaz@gmail.com>
2025-05-25 01:21:56 +0200

    V0.2.0

--- refresh-theme.py
+++ refresh-theme.py
@@ -3,10 +3,15 @@ import subprocess
 import time
 import inotify.adapters
 import wofitheme
+from datetime import datetime
+
+LOG_FILE = os.path.expanduser("~/.cache/theme_changer_errors.log")
+
 
 def log_error(message, error):
-    with open(os.path.expanduser("~/.cache/theme_changer_errors.log"), "a") as log_file:
-        log_file.write(f"{message}: {error}\n")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(LOG_FILE, "a") as log_file:
+        log_file.write(f"[{timestamp}] {message}: {error}\n")
     print(f"{message}: {error}")
 
 
@@ -32,6 +37,88 @@ def command_exists(command):
     return subprocess.call(["which", command], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) == 0
 
 
+def run_if_exists(path):
+    path = os.path.expanduser(path)
+    if os.path.exists(path) and os.access(path, os.X_OK):
+        try:
+            subprocess.run([path], check=True)
+        except subprocess.CalledProcessError as e:
+            log_error(f"Errore nell'esecuzione di {path}", e)
+
+
+def apply_terminal_themes():
+    if command_exists("kitty") and file_exists("~/.cache/wal/colors-kitty.conf"):
+        try:
+            subprocess.run(["kitty", "@", "set-colors", "-a", os.path.expanduser("~/.cache/wal/colors-kitty.conf")], check=True)
+        except subprocess.CalledProcessError as e:
+            log_error("Errore nell'applicare il tema a kitty", e)
+
+    if command_exists("fish") and file_exists("~/.cache/wal/colors.fish"):
+        try:
+            subprocess.run(["fish", "-c", "source ~/.cache/wal/colors.fish"], check=True)
+        except subprocess.CalledProcessError as e:
+            log_error("Errore nell'applicare il tema a fish", e)
+
+    if command_exists("alacritty") and file_exists("~/.cache/wal/colors-alacritty.yml"):
+        try:
+            subprocess.run(["alacritty", "--config-file", os.path.expanduser("~/.cache/wal/colors-alacritty.yml")], check=True)
+        except subprocess.CalledProcessError as e:
+            log_error("Errore nell'applicare il tema ad Alacritty", e)
+
+
+def apply_desktop_themes():
+    try:
+        subprocess.run(["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", "FlatColor"], check=True)
+    except subprocess.CalledProcessError as e:
+        log_error("Errore nell'applicare il tema GTK", e)
+
+    os.environ["QT_STYLE_OVERRIDE"] = "kvantum"
+
+    if file_exists("~/.cache/wal/colors-firefox.css"):
+        firefox_profile = os.path.expanduser("~/.mozilla/firefox/*.default-release/chrome/")
+        try:
+            subprocess.run(["cp", "-r", os.path.expanduser("~/.cache/wal/colors-firefox.css"), firefox_profile], check=True)
+        except subprocess.CalledProcessError as e:
+            log_error("Errore nell'applicare il tema a Firefox", e)
+
+    if file_exists("~/.cache/wal/colors.json"):
+        try:
+            # wofitheme.apply_theme()
+            pass
+        except Exception as e:
+            log_error("Errore nell'applicare il tema a Wofi", e)
+
+
+def apply_bar_themes():
+    run_if_exists("~/Code/YuriLand/YuriTheme/css-to-scss-waybar.sh")
+    run_if_exists("~/Code/YuriLand/YuriTheme/css-to-scss-swaync.sh")
+    run_if_exists("~/Code/YuriLand/YuriTheme/make-rofi-theme.sh")
+
+    if file_exists("~/.cache/wal/colors-foot.ini"):
+        try:
+            subprocess.run(["cp", os.path.expanduser("~/.cache/wal/colors-foot.ini"), os.path.expanduser("~/.config/foot/")], check=True)
+        except subprocess.CalledProcessError as e:
+            log_error("Errore nell'applicare il tema a Foot", e)
+
+
+def reload_bars():
+    if command_exists("pkill"):
+        try:
+            subprocess.run(["pkill", "-USR1", "ironbar"], check=True)
+        except subprocess.CalledProcessError as e:
+            log_error("Errore nel ricaricare Ironbar", e)
+
+        try:
+            subprocess.run(["pkill", "-USR1", "waybar"], check=True)
+            subprocess.run(['zsh', '-c', "bgp waybar"], capture_output=True, text=True)
+        except subprocess.CalledProcessError as e:
+            log_error("Errore nel ricaricare Waybar", e)
+
+
+def update_starship_colors():
+    run_if_exists("~/Code/YuriLand/YuriTheme/update_starship_colors.sh")
+
+
 def apply_theme():
     wallpaper = get_wallpaper()
     if not wallpaper:
@@ -45,83 +132,11 @@ def apply_theme():
         log_error("Errore nell'applicare il tema con pywal", e)
         return
 
-    try:
-        subprocess.run(["gsettings", "set", "org.gnome.desktop.interface", "gtk-theme", "FlatColor"], check=True)
-    except subprocess.CalledProcessError as e:
-        log_error("Errore nell'applicare il tema GTK", e)
-
-    os.environ["QT_STYLE_OVERRIDE"] = "kvantum"
-
-    if command_exists("kitty") and file_exists("~/.cache/wal/colors-kitty.conf"):
-        try:
-            subprocess.run(["kitty", "@", "set-colors", "-a", os.path.expanduser("~/.cache/wal/colors-kitty.conf")],
-                           check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a kitty", e)
-
-    if command_exists("fish") and file_exists("~/.cache/wal/colors.fish"):
-        try:
-            subprocess.run(["fish", "-c", "source ~/.cache/wal/colors.fish"], check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a fish", e)
-
-    if command_exists("alacritty") and file_exists("~/.cache/wal/colors-alacritty.yml"):
-        try:
-            subprocess.run(["alacritty", "--config-file", os.path.expanduser("~/.cache/wal/colors-alacritty.yml")],
-                           check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema ad Alacritty", e)
-
-    firefox_profile = os.path.expanduser("~/.mozilla/firefox/*.default-release/chrome/")
-    if file_exists("~/.cache/wal/colors-firefox.css"):
-        try:
-            subprocess.run(["cp", "-r", os.path.expanduser("~/.cache/wal/colors-firefox.css"), firefox_profile],
-                           check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a Firefox", e)
-
-    if file_exists("~/.cache/wal/colors.json"):
-        try:
-            #wofitheme.apply_theme()
-            pass
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a Wofi", e)
-
-    if file_exists("~/.cache/wal/colors-waybar.css"):
-        try:
-            subprocess.run([os.path.expanduser("~/Code/YuriLand/YuriTheme/css-to-scss-waybar.sh")], check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a Waybar", e)
-
-    if file_exists("~/.cache/wal/colors-foot.ini"):
-        try:
-            subprocess.run(["cp", os.path.expanduser("~/.cache/wal/colors-foot.ini"), os.path.expanduser("~/.config/foot/")], check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a Foot", e)
-
-    if file_exists("~/.cache/wal/colors-waybar.css"):
-        try:
-            subprocess.run([os.path.expanduser("~/Code/YuriLand/YuriTheme/css-to-scss-swaync.sh")], check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a SwayNC", e)
-
-    if file_exists("~/.cache/wal/colors-rofi-dark.rasi"):
-        try:
-            subprocess.run([os.path.expanduser("~/Code/YuriLand/YuriTheme/make-rofi-theme.sh")], check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nell'applicare il tema a Rofi", e)
-
-    if command_exists("pkill"):
-        try:
-            subprocess.run(["pkill", "-USR1", "ironbar"], check=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nel ricaricare Ironbar", e)
-
-        try:
-            subprocess.run(["pkill", "-USR1", "waybar"], check=True)
-            subprocess.run(['zsh', '-c', "bgp waybar"], capture_output=True, text=True)
-        except subprocess.CalledProcessError as e:
-            log_error("Errore nel ricaricare Waybar", e)
+    apply_desktop_themes()
+    apply_terminal_themes()
+    apply_bar_themes()
+    update_starship_colors()
+    reload_bars()
 
     print("Tema aggiornato!")
     print("Monitoraggio delle modifiche dello sfondo...")
@@ -131,6 +146,10 @@ def main():
     swww_cache = os.path.expanduser("~/.cache/swww/eDP-1")
     if not os.path.exists(swww_cache):
         log_error("Errore", "Il file di cache di swww non esiste. Assicurati che swww sia in esecuzione.")
+        return
+
+    if "--once" in os.sys.argv:
+        apply_theme()
         return
 
     try:
