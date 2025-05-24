b84c6c1e55495382ed636db16a35efa83699f2de
lonelyobserver <lcmulaz@gmail.com>
2025-05-25 01:29:34 +0200

    V0.3.0

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import subprocess
-
-def file_exists(path):
-    return os.path.exists(os.path.expanduser(path))
-
-subprocess.run(["cp", "-r", os.path.expanduser("~/.cache/wal/colors-rofi.rasi"),
-                            os.path.expanduser("~/.config/wofi/style.css")], check=True)
